8566ba4a6f5f0407b5cc6e256fa0626905ec967e
Marcel Arie <googlillo@gmail.com>
2022-04-16 23:19:50 +0200

    first commit

---
+++ home.nix
@@ -0,0 +1,246 @@
+{ config, pkgs, ... }:
+
+{
+  imports = [
+    ./programs/kitty.nix
+    ./programs/fish
+    ./programs/bash
+    ./programs/vim
+  ];
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+  # services.picom.enable = true;
+
+  # Home Manager needs a bit of information about you and the
+  # paths it should manage.
+  home.username = "marcel";
+  home.homeDirectory = "/home/marcel";
+
+  nixpkgs.config.allowUnfree = true;
+
+  programs.neovim = {
+    enable = true;
+    package = pkgs.neovim-nightly;
+    viAlias = true;
+    withPython3 = true;
+    extraConfig = ''luafile /home/marcel/.config/nvim/nix.init.lua'';
+
+    extraPackages = with pkgs; [
+      tree-sitter
+      pkgs.rnix-lsp
+      nodePackages.typescript
+      nodePackages.typescript-language-server
+      nodePackages.bash-language-server
+      nodePackages.vscode-langservers-extracted
+      nodePackages.vim-language-server
+      nodePackages.typescript
+      nodePackages.typescript-language-server
+      nodePackages.intelephense
+      nodePackages.dockerfile-language-server-nodejs
+      nodePackages.yaml-language-server
+      sumneko-lua-language-server
+      texlab
+      rust-analyzer
+      deno
+      stylua
+      nodePackages.prettier
+      nodePackages.prettier_d_slim
+      perl534Packages.PerlTidy
+      perl534Packages.PerlCritic
+      perlPackages.PLS
+      sqlite
+      fzf
+
+      # nur.repos.crazazy.efm-langserver
+    ];
+  };
+
+  programs.git = {
+    enable = true;
+    userName = "Marcel Arie";
+    userEmail = "googlillo@gmail.com";
+    # aliases = { prettylog = "..."; };
+    extraConfig = {
+      core = {
+        editor = "nvim";
+      };
+      color = {
+        ui = true;
+      };
+      init = {
+        defaultBranch = "main";
+      };
+      # push = { default = "simple"; };
+      # pull = { ff = "only"; };
+    };
+    # ignores = [ ".DS_Store" "*.pyc" ];
+    delta = {
+      enable = true;
+      options = {
+        navigate = true;
+        line-numbers = true;
+        # syntax-theme = "calochortus-lyallii";
+      };
+    };
+  };
+
+  gtk = {
+    enable = true;
+    theme = {
+      name = "Materia-dark";
+      package = pkgs.materia-theme;
+    };
+    iconTheme = {
+      name = "Paper";
+      package = pkgs.paper-icon-theme;
+    };
+    cursorTheme = {
+      package = pkgs.paper-icon-theme;
+      name = "Paper";
+      size = 48;
+    };
+    font = {
+      name = "Noto Sans 9.5";
+      package = pkgs.noto-fonts;
+    };
+  };
+
+  xdg.mimeApps = {
+    enable = true;
+    # associations.added = {
+    #   "application/pdf" = [ "org.gnome.Evince.desktop" ];
+    # };
+    defaultApplications = {
+      # "application/pdf" = [ "org.gnome.Evince.desktop" ];
+      "x-scheme-handler/http" = [ "firefox.desktop" ];
+      "x-scheme-handler/https" = [ "firefox.desktop" ];
+    };
+  };
+
+  programs.rofi = {
+    enable = true;
+    font = "FiraCode 25";
+    theme = ./programs/rofi/themes/slate.rasi;
+  };
+
+  home.packages = with pkgs;  [
+    tcpdump
+    firefox
+    termite
+    # alacritty
+    rustup
+    polybar
+    pkg-config
+    zoxide
+    starship
+    exa
+    neofetch
+    volumeicon
+    sxhkd
+    haskellPackages.greenclip
+    feh
+    rofi
+    skim
+    pavucontrol
+    ponymix
+    unzip
+    arandr
+    autorandr
+    bat
+    ripgrep
+    lua
+    lua53Packages.luarocks
+    python3
+    fd
+    sad
+    gh
+    python3
+    nodejs-12_x
+    flameshot
+    rofi-power-menu
+    webcamoid
+    nixpkgs-fmt
+    slack
+    docker-compose
+    libnotify
+    rofimoji
+    sysz
+    tldr
+    nix-prefetch-github
+    glow
+    element-desktop
+    mitmproxy
+    xfce.xfce4-notifyd
+    sumneko-lua-language-server
+    kazam
+    mpv
+    yt-dlp
+    pfetch
+    discord
+    python39Packages.pynvim
+    tridactyl-native
+    jq
+    mesa.drivers
+    signal-desktop
+    (yarn.override { nodejs = null; })
+    update-systemd-resolved
+    htop
+    mongodb
+    mongodb-tools
+    nodePackages.speed-test
+    nodePackages.pnpm
+    fzf
+    fzy
+    chromium
+    brave
+    hugo
+    cht-sh
+    keepass
+    dmenu
+    swaylock
+    swayidle
+    wl-clipboard
+    mako
+    dmenu
+    gimp
+    das_watchdog
+    acpi
+    lf
+    xorg.xev
+    qbittorrent
+    auto-cpufreq
+    cbatticon
+    powertop
+    xfce.thunar
+    manix
+    cowsay
+    autoconf
+    libtool
+    automake
+    node2nix
+    moreutils
+    entr
+    zk
+
+    # NUR
+    nur.repos.afreakk.mongosh
+
+    # chromium
+    # leftwm
+    # chromium
+    # dunst
+    # update-systemd-resolved
+    # openvpn
+    # home-manager
+    # fish
+    # vim
+    # cargo
+    # sumneko-lua-language-server
+  ];
+
+  programs.java.enable = true;
+
+  # home.stateVersion = "21.05";
+  # home.stateVersion = "20.09";
+}
---
+++ programs/fish/default.nix
@@ -0,0 +1,99 @@
+{ pkgs, config, lib, ... }:
+
+{
+  config.programs.fish = {
+    enable = true;
+    shellInit = builtins.readFile ./prenix-config.fish;
+    functions = {
+      gld = ''
+        set branch (git rev-parse --abbrev-ref HEAD)
+        git fetch \
+            && git log --color -p --full-diff $branch..origin/$branch \
+            && git merge origin/$branch
+      '';
+      gitignore = "curl -sL https://www.gitignore.io/api/$argv";
+      goo = "xdg-open \"https://www.google.com/search?q=$argv\" ";
+      curent_branch = "git symbolic-ref --short HEAD";
+      hms = "cd ~/.config/nixos/; git add --all; home-manager switch --flake . $argv; cd -";
+      hmb = "cd ~/.config/nixos/; git add --all; home-manager build --flake . $argv; cd -";
+      trs = ''
+        if test -z "$argv"
+              curl -s -X GET "https://libretranslate.com/languages" \
+                  -H  "accept: application/json"  | jq -j '.[]|"\n", .code, " (",.name, ") "'
+            return 1;
+        end
+          curl -s 'https://libretranslate.de/translate' \
+                 -H 'Content-Type: application/json' \
+                 -d "{\"q\":\"$argv[1..-3]\",\"source\":\"$argv[-2]\",\"target\":\"$argv[-1]\"}" \
+                 | jq -r .translatedText
+      '';
+    };
+    shellAliases = import ../aliases.nix;
+    plugins = [
+      {
+        name = "foreign-env";
+        src = pkgs.fetchFromGitHub {
+          owner = "oh-my-fish";
+          repo = "plugin-foreign-env";
+          rev = "dddd9213272a0ab848d474d0cbde12ad034e65bc";
+          sha256 = "00xqlyl3lffc5l0viin1nyp819wf81fncqyz87jx8ljjdhilmgbs";
+        };
+      }
+      {
+        name = "z";
+        src = pkgs.fetchFromGitHub {
+          owner = "jethrokuan";
+          repo = "z";
+          rev = "e0e1b9dfdba362f8ab1ae8c1afc7ccf62b89f7eb";
+          sha256 = "0dbnir6jbwjpjalz14snzd3cgdysgcs3raznsijd6savad3qhijc";
+        };
+      }
+      {
+        name = "base16-fish";
+        src = pkgs.fetchFromGitHub {
+          owner = "tomyun";
+          repo = "base16-fish";
+          rev = "2f6dd973a9075dabccd26f1cded09508180bf5fe";
+          sha256 = "142fmqm324gy3qsv48vijm5k81v6mw85ym9mmhnvyv2q2ndg5rix";
+        };
+      }
+      {
+        name = "bang-bang";
+        src = pkgs.fetchFromGitHub {
+          owner = "oh-my-fish";
+          repo = "plugin-bang-bang";
+          rev = "f969c618301163273d0a03d002614d9a81952c1e";
+          sha256 = "1r3d4wgdylnc857j08lbdscqbm9lxbm1wqzbkqz1jf8bgq2rvk03";
+        };
+      }
+      {
+        name = "nvm";
+        src = pkgs.fetchFromGitHub {
+          owner = "jorgebucaran";
+          repo = "nvm.fish";
+          rev = "81170ef5bc127af4622aa456122c77f363f247bc";
+          sha256 = "0ra0yj8nksza1j84l5v5063w0cyxwwb7sxqggf2rlh3i3j77ldz6";
+        };
+      }
+      {
+        name = "nvm";
+        src = pkgs.fetchFromGitHub {
+          owner = "jorgebucaran";
+          repo = "nvm.fish";
+          rev = "81170ef5bc127af4622aa456122c77f363f247bc";
+          sha256 = "0ra0yj8nksza1j84l5v5063w0cyxwwb7sxqggf2rlh3i3j77ldz6";
+          fetchSubmodules = true;
+        };
+      }
+      {
+        name = "bass";
+        src = pkgs.fetchFromGitHub {
+          owner = "edc";
+          repo = "bass";
+          rev = "2fd3d2157d5271ca3575b13daec975ca4c10577a";
+          sha256 = "0mb01y1d0g8ilsr5m8a71j6xmqlyhf8w4xjf00wkk8k41cz3ypky";
+        };
+      }
+    ];
+  };
+}
